cc9869e3d2e2d2d57f50c44f47de521099ee487e
FLJoergensen (4joergen) <fljoergensen1994@googlemail.com>
2016-01-21 13:13:09 +0100

    11_2
    +3

--- SE3-funktional/se3_11_2.rkt
+++ SE3-funktional/se3_11_2.rkt
@@ -1,12 +1,17 @@
 #lang lazy
 
-(require racket/format)
+(require racket/format
+         se3-bib/tools-module)
 ;2
 (define (BöseSieben x)
   (cond ((= 0 (modulo x 7)) (cons 'sum (BöseSieben (+ x 1))))
         ((member #\7 (string->list (~a x))) (cons 'sum (BöseSieben (+ x 1))))
         (else (cons x (BöseSieben (+ x 1))))))
 ;3
-(define (fib x)
-  (cond ((< x 2) 1)
-        (else (cons (+ (fib (- x 1)) (fib (- x 2))) (fib (+ x 1))))))
+(define (Fib x)
+  (cond ((= x 0) 0)
+        ((< x 3) 1)
+        (else (+ (Fib (- x 1)) (Fib (- x 2))))))
+
+(define memoFib (memo Fib))
+(set! Fib memoFib)
