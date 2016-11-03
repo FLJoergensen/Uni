288b99b0e80d60267fc3d218cef499815cd9ead4
FLJoergensen (4joergen) <fljoergensen1994@googlemail.com>
2016-11-03 12:43:03 +0100

    B02
    A3.1+3.2

--- SE3-Logik/02/SE3LP-G9-A02_DFF.pl
+++ SE3-Logik/02/SE3LP-G9-A02_DFF.pl
@@ -12,7 +12,6 @@
 
 
 %%% A1 %%%
-
 /*
 mutter_von(P1,X),vater_von(X,P2).
 P1 ist die Oma von P2
@@ -32,8 +31,31 @@ P1 und P2 sind halbgeschwister (Gleicher Vater unterschidliche Mutter)
 
 %%% A3 %%%
 
+%%1.
+%anschluss(NR,Name)
+anschluss(0,otter)
+anschluss(1,hans)
+anschluss(2,peter)
+anschluss(3,hellen)
+anschluss(4,petra)
+
+%ip(ip,von,bis,anschlussNR)
+ip(0.0.0.1,20120101,20130101,0)
+ip(0.2.0.0,20120101,20120307,1)
+ip(0.0.1.0,20120307,20130101,1)
+ip(10.0.0.1,20120101,20130101,4)
+ip(0.10.0.1,20120101,20121120,3)
+ip(0.10.3.1,20121120,20130101,3)
+ip(0.10.66.1,20120301,20120701,2)
+
+
+%connection(ip,ip,zeitpunk)
+connection(0.0.0.1,0.2.0.0,20120203)
+connection(10.0.0.1,0.10.66.1,20120203)
+connection(0.10.0.1,10.0.0.1,20120403)
+
+%%3.
 
 %%% A4 %%%
 
 
-%%% A5 %%%
