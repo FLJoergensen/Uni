d81c7c618569c5d0094e3e4106a88df64dc8b8ce
fabianbehrendt <fabian.behrendt95@gmail.com>
2016-11-03 12:12:02 +0100

    Matrikelnummer + Mailadresse vollständig

--- SE3-Logik/SE3LP-G9-A02_DFF.pl
+++ SE3-Logik/SE3LP-G9-A02_DFF.pl
@@ -3,7 +3,7 @@
 % Aufgabenblatt 01 - SE3-LP WiSe 16/17
 % 
 % Finn-Lasse Jörgensen 6700628 4joergen@informatik.uni-hamburg.de
-% Fabian Behrend
+% Fabian Behrendt 6534523 fabian.behrendt95@gmail.com
 % Daniel Klotzsche 6535732 daniel_klotzsche@hotmail.de
 %
 % Wir sind bereit folgende Aufgaben zu präsentieren:
