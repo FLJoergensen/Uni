0700662b0617234e6d072caaca0b38aa986f8780
FLJoergensen (4joergen) <fljoergensen1994@googlemail.com>
2016-11-03 12:13:37 +0100

    .

--- SE3-Logik/02/SE3LP-G9-A02_DFF.pl
+++ SE3-Logik/02/SE3LP-G9-A02_DFF.pl
@@ -1,6 +1,6 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
-% Aufgabenblatt 01 - SE3-LP WiSe 16/17
+% Aufgabenblatt 02 - SE3-LP WiSe 16/17
 % 
 % Finn-Lasse Jörgensen 6700628 4joergen@informatik.uni-hamburg.de
 % Fabian Behrend
@@ -11,6 +11,19 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %A1
+*/
+mutter_von(P1,X),vater_von(X,P2).
+P1 ist die Oma von P2
+
+mutter_von(X,P1),mutter_von(Y,X),mutter_von(Y,P2),X\=P2.
+P2 ist die\der Tante\Onkel von P1
+
+vater_von(X,P1),mutter_von(Y,X),mutter_von(Y,Z), mutter_von(Z,P2),X\=Z.
+P1 ist der\die Cosain\Cosine von P2
+
+mutter_von(X,P1),mutter_von(Y,P2),vater_von(Z,P1), vater_von(Z,P2),P1\=P2,X\=Y.
+P1 und P2 sind halbgeschwister (Gleicher Vater unterschidliche Mutter)
+/*
 
 
 %A2
