bc226dad3849e824fa65cca4d51abb225d910a3a
FLJoergensen (4joergen) <fljoergensen1994@googlemail.com>
2016-11-08 08:01:22 +0100

    B02
    Presentation

--- SE3-Logik/02/SE3LP-G9-A02_DFF.pl
+++ SE3-Logik/02/SE3LP-G9-A02_DFF.pl
@@ -6,7 +6,7 @@
 % Fabian Behrendt 6534523 fabian.behrendt95@gmail.com
 % Daniel Klotzsche 6535732 daniel_klotzsche@hotmail.de
 %
-% Wir sind bereit folgende Aufgaben zu präsentieren:
+% Wir sind bereit folgende Aufgaben zu präsentieren:A1,A3,A2.1,A2.2,A4
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 [haeuser]
