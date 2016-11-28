9a0ab464c1671865e52fbdf68da5fe7309a0f373
Daniel <Daniel@192.168.1.64>
2016-11-28 15:59:17 +0100

    Blatt 5 A1

--- SE3-Logik/05/SE3LP-G9-A05_DFF.pl
+++ SE3-Logik/05/SE3LP-G9-A05_DFF.pl
@@ -12,6 +12,39 @@
 
 %%% A1 %%%
 
+% a(B, C) = a(m, p) ?
+% a/2 = a/2
+% B = m, C = p
+% a(m, p) = a(m, p)
+% Unifikation erfolgreich!
+
+% s(1, 2) = s(P, P) ? 
+% s/2 = s/2
+% P = 1
+% Unifizieren fehlgeschlagen
+% P wird bereits mit 1 belegt und kann nicht mehr mit 2 belegt werden!
+
+% g(f(s, R),f(R, s)) = g(f(S, t(T)), f(t(t), S)) ?
+% g/2 = g/2
+% g(f/2, f/2) = g(f/2, f/2)
+% S = s, R = t(T), t(T) = t(t)
+% g(f(s, t(t)), f(t(t), s)) = g(f(s, t(t)), f( t(t), s))
+% Unifikation erfolgreich!
+
+% q(t(r,s),c(g),h(g(T)),t) = q(Y,c(f(r,T)),h(Y)) ?
+% q/4 =/= q/3
+% Unifizieren fehlgeschlagen
+% q/4 und q/3 lassen sich nicht Unifizieren, weil sie nicht zusammen passen!
+
+% true = not(not(True)) ?
+% not(not(True) = true
+% true = true
+% Unifikation erfolgreich!
+
+% True = not(false) ?
+% not(false) = true, True = true
+% true = true
+% Unifikation erfolgreich
 
 %%% A2 %%%
 
